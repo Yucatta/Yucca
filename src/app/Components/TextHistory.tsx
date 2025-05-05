bb73baa0c947761929db41abe474d76c996d24de
Yucatta <ahmetabali41@gmail.com>
2025-05-06 00:01:29 +0300

    something something

--- src/app/Components/TextHistory.tsx
+++ src/app/Components/TextHistory.tsx
@@ -10,31 +10,49 @@ interface Props {
 }
 const TextHistory = ({ texthistory, User }: Props) => {
   return (
-    <div className="w-full h-5/6  ">
+    <div className="w-full h-5/6 flex flex-col ">
       {texthistory.map((messageinfo, index) => {
         console.log(messageinfo.Sender, User, "aaaa");
-        const classname =
-          messageinfo.Sender === User
-            ? "flex flex-row items-end"
-            : "flex flex-row-reverse items-start";
-        // console.log(classname, messageinfo.Time.getHours());
-        const hours = new Date(messageinfo.Time).getHours();
+        // cons;ole.log(classname, messageinfo.Time.getHours());
+        const date = new Date(messageinfo.Time);
+        // const hours = new Date(messageinfo.Time).getHours();
+        // const minutes = new Date(messageinfo.Time).getHours();
+        const leftrem = messageinfo.Message.length * 4;
+        console.log(leftrem);
+        console.log(
+          `text-xs relative left-[${messageinfo.Message.length}px] top-6 text-neutral-400`
+        );
         return messageinfo.Sender === User ? (
-          <div className={classname} key={index}>
+          <div className={"flex flex-col items-end"} key={index}>
             <li
               className={
-                messageinfo.Sender === User
-                  ? "w-auto border-1 rounded-xl h-10 mt-3 flex justify-center px-3 bg-gray-600"
-                  : "w-auto border-1 rounded-xl h-10 mt-3 flex justify-center px-3 bg-gray-900"
+                "w-auto border-1 rounded-xl h-10 mt-3 flex justify-center px-3 bg-gray-600"
               }
               // onClick={() => handleClickingPerson(person)}
             >
               {messageinfo.Message}
-              <span className="text-xs">{`${hours}`}</span>
+              <span
+                className={`text-xs relative  top-6 text-neutral-400`}
+                // style={{ left: `${leftrem * 1.5 + 30}px` }}
+              >{`${date.getHours()}:${date.getMinutes()}`}</span>
             </li>
           </div>
         ) : (
-          ""
+          <div className={"flex  "} key={index}>
+            <li
+              className={
+                "w-auto border-1  rounded-xl h-10 mt-3 flex justify-center px-3 bg-gray-900"
+              }
+              // onClick={() => handleClickingPerson(person)}
+            >
+              <span>{messageinfo.Message}</span>
+              <span
+                //    right-[${messageinfo.Message.length}rem]
+                className={`text-xs relative top-6  text-neutral-400`}
+                // style={{ left: `${leftrem * 1.5 + 30}px` }}
+              >{`${date.getHours()}:${date.getMinutes()}`}</span>
+            </li>
+          </div>
         );
       })}
     </div>
